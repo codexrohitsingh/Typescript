afa226433c3b370b179c5c835a11e7baf3b57633
rohit singh <codex.rohit.singh@gmail.com>
2025-10-21 07:40:20 +0530

    .

---
+++ node-app/a.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let x = 12;
+x = "number";
+console.log(x);
+//# sourceMappingURL=a.js.map
---
+++ node-app/a.ts
@@ -0,0 +1,4 @@
+let x:number= 12; 
+x = "number";
+console.log(x)
+xnos
